787cfad525fc1e0a5f65f34037990071af885020
Gilbert Adu <gea33@cornell.edu>
2024-03-30 13:17:06 -0400

    new config added

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_kOJWv2n2m', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1kehajf3hruu455egrj4tlea6p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
